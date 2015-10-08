ea1146dd256b88182992a5adb4d674c5d1b0ca6e
juanra <1989juanra@gmail.com>
2015-10-08 21:38:00 +0200

    hola

---
+++ quepasa2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+def decision(a,b):
+	if a > b:
+		salida = "es mayor que b"
+	else:
+		salida = "b es mayor que a y lo subo a github"
+		salida = salida + "o igual"
+	print "fin del tema"
+	return salida
